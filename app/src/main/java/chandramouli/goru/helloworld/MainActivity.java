985dc299cacca6dc1c7ca4b5de8af032b5bdaa9d
mouli1382 <mouli1382@gmail.com>
2016-10-14 07:42:54 +0530

    Added Digits auth

--- app/src/main/java/chandramouli/goru/helloworld/MainActivity.java
+++ app/src/main/java/chandramouli/goru/helloworld/MainActivity.java
@@ -5,12 +5,22 @@ import android.os.AsyncTask;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.digits.sdk.android.AuthCallback;
+import com.digits.sdk.android.DigitsAuthButton;
+import com.digits.sdk.android.DigitsException;
+import com.digits.sdk.android.DigitsSession;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.digits.sdk.android.Digits;
+import io.fabric.sdk.android.Fabric;
 
 import java.io.IOException;
 
@@ -18,10 +28,35 @@ import chandramouli.goru.helloworld.backend.myApi.MyApi;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "iHf1tJ7TsFZM7rKDzeFtz91Qk";
+    private static final String TWITTER_SECRET = "bpnXLf3P1YupEW74rrRJySlFhs0bjacGKr6lJvdVatOuqPPPTu";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new TwitterCore(authConfig), new Digits.Builder().build());
+
         setContentView(R.layout.activity_main);
+
+
+        DigitsAuthButton digitsButton = (DigitsAuthButton) findViewById(R.id.auth_button);
+        digitsButton.setCallback(new AuthCallback() {
+            @Override
+            public void success(DigitsSession session, String phoneNumber) {
+                // TODO: associate the session userID with your user model
+                Toast.makeText(getApplicationContext(), "Authentication successful for "
+                        + phoneNumber, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void failure(DigitsException exception) {
+                Log.d("Digits", "Sign in with Digits failure", exception);
+            }
+        });
+
 
         new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Chandra!"));
     }
